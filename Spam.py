63e00b84809470d65f9b1e72f31cd76e49035be4
Aaron Crosbie <aaroncrosie@outlook.com>
2020-11-09 20:55:02 +0000

    customisation added

--- Spam.py
+++ Spam.py
@@ -5,22 +5,52 @@ import time
 def beeM():
     time.sleep(5)
     f = open('BeeMovie.txt', 'r')
+    i = 0
     for r in f:
+        i = i+1
         pyautogui.typewrite(r)
         pyautogui.press('enter')
-        time.sleep(0.6)
+        time.sleep(0.8)
+        print(i)
+
     f.close()
 
 
 def ping():
     who = input('who to ping?')
     num = int(input('how many times?'))
+    message = input('message?')
     time.sleep(5)
     for i in range(num):
-        pyautogui.typewrite('@')
-        pyautogui.typewrite(who)
-        pyautogui.press('enter')
+        if who == 'everyone':
+            if message == 'n':
+                pyautogui.typewrite('@')
+                pyautogui.typewrite(who)
+                pyautogui.press('enter')
+            else:
+                pyautogui.typewrite('@')
+                pyautogui.typewrite(who)
+                pyautogui.typewrite(' ')
+                pyautogui.typewrite(message)
+                pyautogui.press('enter')
+
+        else:
+            if message == 'n':
+                pyautogui.typewrite('@')
+                pyautogui.typewrite(who)
+                pyautogui.press('enter')
+                pyautogui.press('enter')
+            else:
+                pyautogui.typewrite('@')
+                pyautogui.typewrite(who)
+                pyautogui.press('enter')
+                pyautogui.typewrite(' ')
+                pyautogui.typewrite(message)
+                pyautogui.press('enter')
+
         time.sleep(0.6)
+        if i == num//2:
+            print(i)
 
 
 def tragedy():
