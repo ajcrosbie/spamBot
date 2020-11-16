872e66a62b283aca5d7ee8df68354fe1522f40e1
Aaron Crosbie <aaroncrosie@outlook.com>
2020-11-16 21:45:04 +0000

    class made

---
+++ SpamGui.py
@@ -0,0 +1,32 @@
+import pyautogui
+import time
+import tkinter as tk
+
+
+class spammer():
+    def __init__(self):
+        self.root = tk.Tk()
+
+    def menu(self):
+        pass
+
+    def Mping(self):
+        pass
+
+    def Mtragedy(self):
+        pass
+
+    def Mmessage(self):
+        pass
+
+    def ping(self):
+        pass
+
+    def tragedy(self):
+        pass
+
+    def message(self):
+        pass
+
+    def bee(self):
+        pass
